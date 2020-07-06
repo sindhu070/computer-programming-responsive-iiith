75fe03b4e5386d80d0d46dd8c93ca85763c7b796
sindhu070 <gorthi.17.it@anits.edu.in>
2020-07-06 16:31:28 +0530

    simulation/exp1

--- src/lab/exp1/simulation/numericalApproximation.js
+++ src/lab/exp1/simulation/numericalApproximation.js
@@ -290,10 +290,18 @@ window.view = {
 			alert('Enter numeric value of a and b');
 			return false;
 		} 
-		else if (valueA2 >= valueB2 || valueB2 > 30) {
-			alert('Integration Limits are from 0 to 30, b > a and b-a >= 1');
+		else if((valueA2 <1 || valueA2>10) || (valueB2 <1 || valueB2>10))
+		{
+			alert("Invalid Input");
+			
 			return false;
 		}
+		else if (valueA2 >= valueB2 || valueB2 > 10) {
+			alert('Integration Limits are from 0 to 10, b > a and b-a >= 1');
+			return false;
+		}
+		
+		
 		else {
 			model.inputValueA = valueA2;
 			model.inputValueB = valueB2;
