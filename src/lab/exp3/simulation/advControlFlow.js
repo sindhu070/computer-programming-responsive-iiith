d7d89226cd1abf530f796ef78c191415ae11fec7
sindhu070 <gorthi.17.it@anits.edu.in>
2020-07-06 16:33:28 +0530

    simulation/exp3

--- src/lab/exp3/simulation/advControlFlow.js
+++ src/lab/exp3/simulation/advControlFlow.js
@@ -34,6 +34,11 @@ window.view = {
  	},
 	getInput: function() {
 		var inputValue = document.getElementById('simpleLoopInput').value
+		if(inputValue>20 || inputValue <0)
+		{
+			alert("Invalid Input")
+			return false;
+		}
 		model.inp = Number(inputValue)
 		this.clearExecutionSection()
 	},
